baf78e8e156c4706539f34ab27692d3527c51ccb
justconnell <ghostofnorthcarolina@gmail.com>
2025-08-28 10:33:33 -0400

    Allow other webslinger packages to extend configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,6 +14,7 @@ class Configuration implements ConfigurationInterface
         $rootNode = $treeBuilder->getRootNode();
         
         $rootNode
+            ->ignoreExtraKeys()
             ->children()
                 ->arrayNode('mailer_factory')
                     ->children()
